9f7833ba1ad0fb7233e4b2445b9135e4e41f96a1
Dharmmey <ebilakundamilola@gail.com>
2021-02-20 22:43:40 +0100

    I just added a new file

---
+++ test python.py
@@ -0,0 +1,144 @@
+# first_num = 15
+# print(first_num)
+# print(type(first_num))
+
+
+# first_string = "This is Jide's plane"
+# print(first_string)
+# print(type(first_string))
+
+
+# tricky_string = "780"
+# print(tricky_string)
+# print(type(tricky_string))
+
+
+
+# first_float = 0.3
+# print(first_float)
+# print(type(first_float))
+
+
+
+# my_bool = True
+# print(my_bool)
+# print(type(my_bool))
+
+
+
+# my_list = [10, "20", 30, 4.0, True]
+# print(my_list)
+# print(type(my_list))
+
+
+
+# my_tuple = (True, "noise", 50, 0.5)
+# print(my_tuple)
+# print(type(my_tuple))
+
+
+
+# my_set = {"Nigeria", "Togo", False, 9.9}
+# print(my_set)
+# print(type(my_set))
+
+
+# my_dict1 = {"Name": "Dayo", "Age": 5}
+# print(my_dict1)
+# print(type(my_dict1))
+
+
+# my_dict2 = {"Name": ["Dayo", "John"], "Age": [5, 10]}
+# print(my_dict2)
+# print(type(my_dict2))
+
+
+
+
+
+first_operand = 19.9
+second_operand = 98
+
+first_operation = first_operand + second_operand 
+# print(first_operation)
+
+
+difference = first_operation - second_operand
+# print(difference)
+
+
+# check_int = 99 == 99.9
+# print(check_int)
+
+# check_float = 5.7 != 7.5
+# print(check_float)
+
+
+# check_amount = 10.8 < 100
+# print(check_amount)
+
+# my_checkpoint = not(6 < 8 and 900 == 899)
+# print(my_checkpoint)
+
+
+# white = 9
+# black = 9
+
+# check_identity = white is not black
+# print(check_identity)
+
+
+# in_example = "Here's an example for membership operator"
+# in_example2 = [True, 707, "sports", "nose mask"]
+# check_member = "sports" not in in_example2
+# print(check_member)
+
+
+# my_string = "The sound of music"
+# desired_char = my_string[-1]
+# print(desired_char)
+
+# desired_slice = my_string[-8 : -3 : ]
+# print(desired_slice)
+
+
+# xyz = 5 + 5
+# my_string = "The {} sound of music".format(xyz)
+# print(my_string)
+
+
+# first_string = "This is 2021"
+# second_string = "and it isn't a leap year."
+
+# full_string = first_string + " " + second_string
+# # print(full_string)
+
+# mod_full_string = full_string.title()
+# # print(mod_full_string)
+
+# check_full_string = full_string.startswith("T")
+# print(check_full_string)
+
+
+
+# import names
+# print(names.get_name())
+
+
+# for a, b in enumerate(range(5)):
+#     a += 1
+#     print(a, b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
